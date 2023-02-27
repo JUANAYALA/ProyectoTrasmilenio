4e986736daf26a432782104cf7044847a5a1a615
juanayala <jpayala88@hotmail.com>
2023-02-27 12:32:51 -0500

    first commit

--- TransRepo/app.py
+++ TransRepo/app.py
@@ -5,10 +5,9 @@ from datetime import datetime
 from flask import send_from_directory
 import os
 
-
-app=Flask(__name__)
-app.secret_key="develoteca "
-mysql=MySQL()
+app = Flask(__name__)
+app.secret_key = "develoteca"
+mysql = MySQL()
 
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -16,281 +15,275 @@ app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'trasmiapp'
 mysql.init_app(app)
 
+
 @app.route('/')
 def inicio():
     return render_template('sitio/index.html')
 
-@app.route('/img/<imagen>')
-def imagenes(imagen):    
-    print(imagen)
-    return send_from_directory(os.path.join('templates/admin/img'),imagen)
-    return send_from_directory(os.path.join('templates/usuarios/img'),imagen)
 
+@app.route('/img/<imagen>')
+def imagenes(imagen):
+    print(imagen)
+    return send_from_directory(os.path.join('templates/admin/img'), imagen)
 
 
 @app.route('/Formulario')
 def form_prin():
-    return render_template('admin/form_prin.html') 
+    return render_template('admin/form_prin.html')
+
 
 @app.route('/FormularioU')
 def form_prinus():
-    return render_template('usuarios/formulaU.html') 
-  
+    return render_template('usuarios/formulaU.html')
 
 
 @app.route('/Login')
 def login():
-
     return render_template('sitio/login.html')
 
 
 @app.route('/sitio/login', methods=['POST'])
 def login_post():
-               
-           
-        _documentolo=request.form['textDoclo']
-        _password=request.form['textpass']
-        print(_documentolo)
-        print(_password)
-         
-        if _documentolo=="1030696488" and _password==_password=="123456":
-            session["login"]=True
-            session["documento"]="Administrador"
-            return redirect("/AdministracionN")   
-        
-        if _documentolo!="1030696488":
-            session["login"]=True
-            session["documento"]="Administrador"
-            return redirect("/NoticiasU")
+    _documentolo = request.form['textDoclo']
+    _password = request.form['textpass']
+    print(_documentolo)
+    print(_password)
 
-        return render_template('sitio/index.html')
+    if _documentolo == "1030696488" and _password == _password == "123456":
+        session["login"] = True
+        session["documento"] = "Administrador"
+        return redirect("/AdministracionN")
 
-        _id=request.form['textDoclo']
-        print(_id)
-        
-        conexion=mysql.connect()
-        cursor=conexion.cursor()
-        cursor.execute("SELECT documento FROM form_registro where documento=%s",(_id))
-        noti=cursor.fetchall()
-        conexion.commit()            
+    if _documentolo != "1030696488":
+        session["login"] = True
+        session["documento"] = "Administrador"
+        return redirect("/NoticiasU")
 
-        if _documentolo==noti:
-            session["login"]=True
-            session["documento"]="Usuario"
-            return redirect("/NoticiasU")
+    return render_template('sitio/index.html')
 
+    _id = request.form['textDoclo']
+    print(_id)
 
-        
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT documento FROM form_registro where documento=%s", _id)
+    noti = cursor.fetchall()
+    conexion.commit()
+
+    if _documentolo == noti:
+        session["login"] = True
+        session["documento"] = "Usuario"
+        return redirect("/NoticiasU")
+
 
 @app.route('/Noticias')
 def Noticias():
-    conexion=mysql.connect()
-    cursor=conexion.cursor()
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
     cursor.execute("SELECT * FROM form_princ")
-    noticia=cursor.fetchall()
+    noticia = cursor.fetchall()
     conexion.commit()
-    #print (noticia)
+    # print (noticia)
 
     return render_template('admin/Noticias.html', noticia=noticia)
 
 
 @app.route('/AdministracionN')
 def Administrador_Not():
-    conexion=mysql.connect()
-    cursor=conexion.cursor()
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
     cursor.execute("SELECT * FROM form_princ")
-    noticia=cursor.fetchall()
+    noticia = cursor.fetchall()
     conexion.commit()
-    #print (noticia)
+    # print (noticia)
 
     return render_template('admin/administracionN.html', noticia=noticia)
 
+
 @app.route('/AdministracionU')
 def Administrador_Usu():
-    conexion=mysql.connect()
-    cursor=conexion.cursor()
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
     cursor.execute("SELECT * FROM form_registro")
-    usuario=cursor.fetchall()
+    usuario = cursor.fetchall()
     conexion.commit()
-    #print (noticia)
+    # print (noticia)
 
     return render_template('admin/administracionU.html', usuario=usuario)
 
- #*********************FORMULARIO USUARIOS****************      
+
+# *********************FORMULARIO USUARIOS****************
 @app.route('/NoticiasU')
 def Noti_usuario():
-    conexion=mysql.connect()
-    cursor=conexion.cursor()
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
     cursor.execute("SELECT * FROM form_princ")
-    noticia=cursor.fetchall()
+    noticia = cursor.fetchall()
     conexion.commit()
-    #print (noticia)
+    # print (noticia)
 
     return render_template('usuarios/NoticiaU.html', noticia=noticia)
-    
-
 
 
 @app.route('/Administracion')
 def Administrador():
-    conexion=mysql.connect()
-    cursor=conexion.cursor()
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
     cursor.execute("SELECT * FROM form_princ")
-    noticia=cursor.fetchall()
+    noticia = cursor.fetchall()
     conexion.commit()
-    #print (noticia)
+    # print (noticia)
 
     return render_template('admin/administracion.html', noticia=noticia)
 
-
-
-    
 
 @app.route('/Cerrarsesion')
 def Logout():
     return render_template('sitio/index.html')
 
+
 @app.route('/Registro')
 def Registro():
     return render_template('sitio/Registro.html')
 
-#*********************FORMULARIO ADM********************************
+
+# *********************FORMULARIO ADM********************************
 
 @app.route('/admin/Noticias/guardar', methods=['POST'])
 def admin_noticias_guardar():
-    _nombre=request.form['textNom']   
-    _apellido=request.form['textApe']
-    _documento=request.form['textDoc']            
-    _telefono=request.form['textTel']   
-    _descripcion=request.form['textDes']
-    _nombreEs=request.form['textNomE']
-    _direccion=request.form['textDir']
-    _clasificacion=request.form['textCla']            
-    _correoelec=request.form['textCor']   
-    _notificacion=request.form['textNot']
-    _fecha=request.form['textFe']
-    _evidencia=request.files['textEvi']
+    _nombre = request.form['textNom']
+    _apellido = request.form['textApe']
+    _documento = request.form['textDoc']
+    _telefono = request.form['textTel']
+    _descripcion = request.form['textDes']
+    _nombreEs = request.form['textNomE']
+    _direccion = request.form['textDir']
+    _clasificacion = request.form['textCla']
+    _correoelec = request.form['textCor']
+    _notificacion = request.form['textNot']
+    _fecha = request.form['textFe']
+    _evidencia = request.files['textEvi']
 
-    tiempo= datetime.now()
-    horaActual=tiempo.strftime("%Y%H%M%S")
+    tiempo = datetime.now()
+    horaActual = tiempo.strftime("%Y%H%M%S")
 
-    if _evidencia.filename!="":
-        nuevoNombre=horaActual+"_"+_evidencia.filename
-        _evidencia.save("templates/admin/img/"+nuevoNombre)
-  
-      
+    if _evidencia.filename != "":
+        nuevoNombre = horaActual + "_" + _evidencia.filename
+        _evidencia.save("templates/admin/img/" + nuevoNombre)
+
     sqlfp = "Insert Into `form_princ` (`id_fp`, `nombre`, `apellido`, `documento`,`telefono`,`descripcion`, `nom_esta`, `dir_esta`, `cla_esta`,`correo`,`not_esta`, `fecha`,`imagen`) Values (Null,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
-    datos=(_nombre,_apellido,_documento,_telefono,_descripcion,_nombreEs,_direccion,_clasificacion,_correoelec,_notificacion,_fecha,nuevoNombre)
+    datos = (
+        _nombre, _apellido, _documento, _telefono, _descripcion, _nombreEs, _direccion, _clasificacion, _correoelec,
+        _notificacion, _fecha, nuevoNombre)
     conexion = mysql.connect()
     cursor = conexion.cursor()
-    cursor.execute(sqlfp,datos)
+    cursor.execute(sqlfp, datos)
     conexion.commit()
 
     return redirect('/AdministracionU')
 
-#*********************FORMULARIOS USUARIOS ****************
+
+# *********************FORMULARIOS USUARIOS ****************
 
 @app.route('/usuarios/Noticias/guardar', methods=['POST'])
 def usuarios_noticias_guardar():
-    _nombre=request.form['textNom']   
-    _apellido=request.form['textApe']
-    _documento=request.form['textDoc']            
-    _telefono=request.form['textTel']   
-    _descripcion=request.form['textDes']
-    _nombreEs=request.form['textNomE']
-    _direccion=request.form['textDir']
-    _clasificacion=request.form['textCla']            
-    _correoelec=request.form['textCor']   
-    _notificacion=request.form['textNot']
-    _fecha=request.form['textFe']
-    _evidencia=request.files['textEvi']
+    _nombre = request.form['textNom']
+    _apellido = request.form['textApe']
+    _documento = request.form['textDoc']
+    _telefono = request.form['textTel']
+    _descripcion = request.form['textDes']
+    _nombreEs = request.form['textNomE']
+    _direccion = request.form['textDir']
+    _clasificacion = request.form['textCla']
+    _correoelec = request.form['textCor']
+    _notificacion = request.form['textNot']
+    _fecha = request.form['textFe']
+    _evidencia = request.files['textEvi']
 
-    tiempo= datetime.now()
-    horaActual=tiempo.strftime("%Y%H%M%S")
+    tiempo = datetime.now()
+    horaActual = tiempo.strftime("%Y%H%M%S")
 
-    if _evidencia.filename!="":
-        nuevoNombre=horaActual+"_"+_evidencia.filename
-        _evidencia.save("templates/admin/img/"+nuevoNombre)
-   
-    sqlfp = "Insert Into `form_princ` (`id_fp`, `nombre`, `apellido`, `documento`,`telefono`,`descripcion`, `nom_esta`, `dir_esta`, `cla_esta`,`correo`,`not_esta`, `fecha`,`imagen`) Values (Null,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
-    datos=(_nombre,_apellido,_documento,_telefono,_descripcion,_nombreEs,_direccion,_clasificacion,_correoelec,_notificacion,_fecha,nuevoNombre)
+    if _evidencia.filename != "":
+        nuevoNombre = horaActual + "_" + _evidencia.filename
+        _evidencia.save("templates/admin/img/" + nuevoNombre)
+
+    sqlfp = "Insert Into `form_princ` (`id_fp`, `nombre`, `apellido`, `documento`,`telefono`,`descripcion`, " \
+            "`nom_esta`, `dir_esta`, `cla_esta`,`correo`,`not_esta`, `fecha`,`imagen`) Values (Null,%s,%s,%s,%s," \
+            "%s,%s,%s,%s,%s,%s,%s,%s);"
+    datos = (
+        _nombre, _apellido, _documento, _telefono, _descripcion, _nombreEs, _direccion, _clasificacion, _correoelec,
+        _notificacion, _fecha, nuevoNombre)
     conexion = mysql.connect()
     cursor = conexion.cursor()
-    cursor.execute(sqlfp,datos)
+    cursor.execute(sqlfp, datos)
     conexion.commit()
 
     return redirect('/NoticiasU')
 
-#**********************REGISTROS************************
+
+# **********************REGISTROS************************
 
 @app.route('/sitio/Registro/guardar', methods=['POST'])
 def sitio_Registro_guardar():
-    _nombre=request.form['textNom']
-    _apellido=request.form['textApe']
-    _documento=request.form['textDoc']            
-    _telefono=request.form['textTel']      
-    _contraseña=request.form['textPs']
-  
-   
-   
+    _nombre = request.form['textNom']
+    _apellido = request.form['textApe']
+    _documento = request.form['textDoc']
+    _telefono = request.form['textTel']
+    _contraseña = request.form['textPs']
+
     sql = "Insert Into `form_registro` (`id_fr`, `nombre`, `apellido`, `documento`,`telefono`,`contraseña`) Values (Null,%s,%s,%s,%s,%s);"
-    datos=(_nombre,_apellido,_documento,_telefono,_contraseña)
+    datos = (_nombre, _apellido, _documento, _telefono, _contraseña)
     conexion = mysql.connect()
     cursor = conexion.cursor()
-    cursor.execute(sql,datos)
+    cursor.execute(sql, datos)
     conexion.commit()
     return redirect('/NoticiasU')
 
 
-#*********************************ELIMINAR FOTOS Y REGISTROS********************************
+# *********************************ELIMINAR FOTOS Y REGISTROS********************************
 
 @app.route('/admin/Noticias/borrar', methods=['POST'])
 def admin_Noticias_borrar():
-
-    _id=request.form['textId']
+    _id = request.form['textId']
     print(_id)
-    
-    conexion=mysql.connect()
-    cursor=conexion.cursor()
-    cursor.execute("SELECT imagen FROM form_princ where id_fp=%s",(_id))
-    noti=cursor.fetchall()
+
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT imagen FROM form_princ where id_fp=%s", (_id))
+    noti = cursor.fetchall()
     conexion.commit()
 
-    if os.path.exists("templates/admin/img/"+str(noti[0][0])):
-        os.unlink("templates/admin/img/"+str(noti[0][0]))
-    
+    if os.path.exists("templates/admin/img/" + str(noti[0][0])):
+        os.unlink("templates/admin/img/" + str(noti[0][0]))
 
-    conexion=mysql.connect()
-    conexion=mysql.connect()
-    cursor=conexion.cursor()
-    cursor.execute("Delete from form_princ where id_fp=%s",(_id))
+    conexion = mysql.connect()
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute("Delete from form_princ where id_fp=%s", (_id))
     conexion.commit()
-   
 
-    return redirect('/AdministracionN')   
+    return redirect('/AdministracionN')
 
 
 @app.route('/admin/Noticias/borrarU', methods=['POST'])
 def admin_Noticias_borrarU():
-
-    _id=request.form['textId']
+    _id = request.form['textId']
     print(_id)
-    
-    conexion=mysql.connect()
-    cursor=conexion.cursor()
-    cursor.execute("SELECT id_fr FROM form_registro where id_fr=%s",(_id))
-    noti=cursor.fetchall()
-    conexion.commit()
-        
 
-    conexion=mysql.connect()
-    conexion=mysql.connect()
-    cursor=conexion.cursor()
-    cursor.execute("Delete from form_registro where id_fr=%s",(_id))
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT id_fr FROM form_registro where id_fr=%s", (_id))
+    noti = cursor.fetchall()
     conexion.commit()
-   
 
-    return redirect('/AdministracionU')  
+    conexion = mysql.connect()
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute("Delete from form_registro where id_fr=%s", (_id))
+    conexion.commit()
+
+    return redirect('/AdministracionU')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
